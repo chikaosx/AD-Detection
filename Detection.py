257611e4a8c06ee5c2194cbe1fd89c5b89dacb20
chikaosx <125705035+chikaosx@users.noreply.github.com>
2024-05-22 11:41:10 +0100

    Update Detection.py

--- Detection.py
+++ Detection.py
@@ -8,8 +8,6 @@ model = pickle.load(open('gradexp.pkl', 'rb'))
 def main(): 
     st.title("Alzheimer's Disease Detection")
 
-    def home_page():
-    st.image(BANNER)
     st.write("""
         ## Introduction to Alzheimer's Disease
         Alzheimer's disease (AD) is a progressive neurodegenerative disease. Though best known for its role in declining memory function, symptoms also include: difficulty thinking and reasoning, making judgements and decisions, and planning and performing familiar tasks. It may also cause alterations in personality and behavior. The cause of AD is not well understood. There is thought to be a significant hereditary component. For example, a variation of the APOE gene, APOE e4, increases risk of Alzheimer's disease.
